f9a8ea2bdca50584021075935b4e2e30cb410ed6
unknown <edgarrios412@gmail.com>
2024-06-15 12:50:27 -0500

    push

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,10 @@ import axios from "axios";
 import { Toaster } from "./components/ui/toaster.jsx";
 import ScrollToTop from "./utils/helpers/scrollToTop.js";
 import { UserProvider } from "./components/context/UserProvider.jsx";
+import MailDesign from "./pages/MailDesign.jsx";
 
-axios.defaults.baseURL = "https://rifavoback.onrender.com";
-// axios.defaults.baseURL = 'http://localhost:3001';
+// axios.defaults.baseURL = "https://rifavoback.onrender.com";
+axios.defaults.baseURL = 'http://localhost:3001';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
@@ -16,6 +17,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Toaster />
       <ScrollToTop />
       <App />
+      {/* <MailDesign/> */}
     </UserProvider>
   </BrowserRouter>
 );
--- src/pages/SorteoDetail.jsx
+++ src/pages/SorteoDetail.jsx
@@ -9,10 +9,12 @@ import { Separator } from "@/components/ui/separator"
 import { toast } from "@/components/ui/use-toast"
 import axios from "axios"
 import ani1 from '../../public/animations/empty.json';
+import winner from '../../public/animations/winner.json';
 import { CalendarDays, ChevronLeft, CreditCard, Ticket, Trophy } from "lucide-react"
 import { useContext, useEffect, useState } from "react"
 import { useNavigate, useParams } from "react-router-dom"
 import Lottie from "lottie-react"
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from "@/components/ui/dialog"
 
 const SorteoDetail = () => {
     const { id } = useParams()
@@ -26,6 +28,8 @@ const SorteoDetail = () => {
     const [paymentMethod, setPaymentMethod] = useState(0)
     const [form, setForm] = useState({})
     const [filter, setFilter] = useState("")
+    const [login, setLogin] = useState(true)
+    const [hayGanadores, setHayGanadores] = useState(false)
 
     const filteredNumeros = numeros.filter(index => index.toString().padStart(3, '0').includes(filter));
 
@@ -85,6 +89,11 @@ const SorteoDetail = () => {
     };
 
     const procesarPago = () => {
+        if (!usuario) return toast({
+            variant: "destructive",
+            title: "Ha ocurrido un error",
+            description: "Debes iniciar sesión para realizar una compra",
+        })
         if (!misNumeros.length) return toast({
             variant: "destructive",
             title: "Ha ocurrido un error",
@@ -109,12 +118,64 @@ const SorteoDetail = () => {
         })
     }
 
+    const registroUsuario = () => {
+        if (form.password !== form.password2) return toast({
+            variant: "destructive",
+            title: "Las contraseñas no coinciden",
+            description: "Asegurate de escribir la contraseña correctamente en ambos inputs",
+        })
+        if (!form.name?.length || !form.lastname?.length || !form.phone?.length || !form.email?.length) return toast({
+            variant: "destructive",
+            title: "Campos incompletos",
+            description: "Debes rellenar todos los campos para poder registrarte",
+        })
+        axios.post("/user", form).then(({ data }) => {
+            setLogin(true)
+            toast({
+                title: "Registro exitoso",
+                description: "Ya puedes acceder a tu cuenta",
+            })
+        }, (e) => toast({
+            variant: "destructive",
+            title: "Ha ocurrido un error",
+            description: e.response.data,
+        }))
+    }
+
     useEffect(() => {
-        axios.get(`/sorteo/listar/${id}`).then(({ data }) => { setSorteo(data); setNumerosComprados(data.tickets.map(t => t.numero)) })
+        axios.get(`/sorteo/listar/${id}`).then(({ data }) => { setSorteo(data); setNumerosComprados(data.tickets.map(t => t.numero)); data.numTicketGanadorP1 && setHayGanadores(true) })
     }, [])
 
     return (
         <>
+            <Dialog open={hayGanadores} onOpenChange={setHayGanadores}>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Tenemos ganadores!</DialogTitle>
+                        <DialogDescription>Felicidades a los ganadores, el equipo de RIFAVO se pondrá en contacto con ustedes en breve!</DialogDescription>
+                    </DialogHeader>
+                    <div className="flex items-center my-10">
+                        <Lottie animationData={winner} style={{ width: "200px", marginTop: "10px" }} loop={false} />
+                        <div>
+                            <p className="font-extrabold text-white bg-gradient-to-r from-yellow-600 via-yellow-500 to-yellow-600 animate-gradient-move w-fit px-3 py-1 rounded-sm">
+                                ¡Premio Mayor!
+                            </p>
+                            <p className="mt-2">Número ganador <b>{sorteo.numTicketGanadorP1}</b></p>
+                            <Separator className="mt-2 mb-3" />
+                            <p className="font-extrabold text-white bg-gradient-to-r from-gray-600 via-gray-500 to-gray-600 animate-gradient-move w-fit px-3 py-1 rounded-sm">
+                                Segundo premio
+                            </p>
+                            <p className="mt-2">Número ganador <b>{sorteo.numTicketGanadorP2}</b></p>
+                            <Separator className="mt-2 mb-3" />
+                            <p className="font-extrabold text-white bg-gradient-to-r from-yellow-900 via-yellow-700 to-yellow-900 animate-gradient-move w-fit px-3 py-1 rounded-sm">
+                                Tercer premio
+                            </p>
+                            <p className="mt-2">Número ganador <b>{sorteo.numTicketGanadorP3}</b></p>
+                        </div>
+                    </div>
+                </DialogContent>
+            </Dialog>
+
             {/* DETALLES DEL SORTEO */}
             <div className="text-start flex flex-col py-24 px-10 lg:px-0 lg:py-0 lg:flex-row items-center justify-center gap-10 lg:gap-40 min-h-[60vh]">
                 <Carrusel imagenes={sorteo?.image?.length > 1 ? [...sorteo.image] : [sorteo.image]} />
@@ -149,7 +210,7 @@ const SorteoDetail = () => {
                         <div>
                             <p className="flex items-center gap-2 font-bold mb-1"><Ticket className="w-5 h-5" />Lotería de Boyacá</p>
                             {sorteo.fechaSorteo ? <p className="text-slate-500 text-sm flex items-center gap-2"><CalendarDays color="black" className="w-5 h-5" />{sorteo.fechaSorteo} 10:40PM</p> : <p className="text-slate-500 text-sm flex items-center gap-2"><CalendarDays color="black" className="w-5 h-5" /> Se anunciará pronto</p>}
-                            {sorteo.numTicketGanadorP1 && <p className="text-slate-500 text-sm flex items-center mt-2 gap-2"><Trophy color="black" className="w-5 h-5" />{sorteo.numTicketGanadorP1}</p>}
+                            {sorteo.numTicketGanadorP1 && <p className="text-lg font-bold flex items-center mt-2 gap-2"><Trophy color="orange" className="w-5 h-5" />{sorteo.numTicketGanadorP1}</p>}
                         </div>
                     </div>
                     <div className="flex flex-col justify-between max-w-56 bg-white rounded-lg p-4 shadow-md hover:border-gray-500 border border-transparent cursor-pointer transition">
@@ -163,7 +224,7 @@ const SorteoDetail = () => {
                         <div>
                             <p className="flex items-center gap-2 font-bold mb-1"><Ticket className="w-5 h-5" />Lotería Cauca</p>
                             {sorteo.fechaSorteo ? <p className="text-slate-500 text-sm flex items-center gap-2"><CalendarDays color="black" className="w-5 h-5" />{sorteo.fechaSorteo} 09:40PM</p> : <p className="text-slate-500 text-sm flex items-center gap-2"><CalendarDays color="black" className="w-5 h-5" /> Se anunciará pronto</p>}
-                            {sorteo.numTicketGanadorP2 && <p className="text-slate-500 text-sm flex items-center mt-2 gap-2"><Trophy color="black" className="w-5 h-5" />{sorteo.numTicketGanadorP2}</p>}
+                            {sorteo.numTicketGanadorP2 && <p className="text-lg font-bold flex items-center mt-2 gap-2"><Trophy color="gray" className="w-5 h-5" />{sorteo.numTicketGanadorP2}</p>}
                         </div>
                     </div>
                     <div className="flex flex-col justify-between max-w-56 bg-white rounded-lg p-4 shadow-md hover:border-yellow-900 border border-transparent cursor-pointer transition">
@@ -177,7 +238,7 @@ const SorteoDetail = () => {
                         <div>
                             <p className="flex items-center gap-2 font-bold mb-1"><Ticket className="w-5 h-5" />Lotería Pijao Noche</p>
                             {sorteo.fechaSorteo ? <p className="text-slate-500 text-sm flex items-center gap-2"><CalendarDays color="black" className="w-5 h-5" />{sorteo.fechaSorteo} 09:00PM</p> : <p className="text-slate-500 text-sm flex items-center gap-2"><CalendarDays color="black" className="w-5 h-5" /> Se anunciará pronto</p>}
-                            {sorteo.numTicketGanadorP3 && <p className="text-slate-500 text-sm flex items-center mt-2 gap-2"><Trophy color="black" className="w-5 h-5" />{sorteo.numTicketGanadorP3}</p>}
+                            {sorteo.numTicketGanadorP3 && <p className="text-lg font-bold flex items-center mt-2 gap-2"><Trophy color="brown" className="w-5 h-5" />{sorteo.numTicketGanadorP3}</p>}
                         </div>
                     </div>
                 </div>
@@ -188,12 +249,12 @@ const SorteoDetail = () => {
                     <h2 className="text-3xl font-bold text-center mb-5">Escoge tus numeros</h2>
                     <Input placeholder="Filtrar tus numeros favoritos" onChange={(e) => setFilter(e.target.value)} />
                     {
-                        !filteredNumeros.length && 
+                        !filteredNumeros.length &&
                         <div className="flex flex-col items-center">
-                        <Lottie animationData={ani1} style={{ width: "150px", marginTop:"40px" }} loop={true} />
-                        <p key={1} className={`flex items-center justify-center text-center mx-auto mt-4 text-slate-500`}>
-                            No hemos conseguido el número que estás buscando
-                        </p>
+                            <Lottie animationData={ani1} style={{ width: "150px", marginTop: "40px" }} loop={true} />
+                            <p key={1} className={`flex items-center justify-center text-center mx-auto mt-4 text-slate-500`}>
+                                No hemos conseguido el número que estás buscando
+                            </p>
                         </div>
                     }
                     <div className="max-h-96 overflow-y-auto overflow-x-hidden select-none mt-10 grid grid-cols-4 sm:grid-cols-7 lg:grid-cols-10 gap-3 items-center">
@@ -215,28 +276,32 @@ const SorteoDetail = () => {
                             <div>
                                 <h2 className="font-bold text-lg mb-4">Ingresa los siguientes datos</h2>
                                 <div>
-                                    <Label>Nombres y Apellidos</Label>
-                                    <Input />
+                                    <Label>Nombre</Label>
+                                    <Input name="name" placeholder="Ingresa tu nombre" />
+                                </div>
+                                <div className="mt-4">
+                                    <Label>Apellido</Label>
+                                    <Input name="name" placeholder="Ingresa tu apellido" />
                                 </div>
                                 <div className="mt-4">
                                     <Label>Correo electrónico</Label>
-                                    <Input />
+                                    <Input name="name" placeholder="Ingresa tu correo" />
                                 </div>
                                 <div className="mt-4">
                                     <Label>Cedula de identidad</Label>
-                                    <Input />
+                                    <Input name="name" placeholder="Ingresa tu cedula de identidad" />
                                 </div>
                                 <div className="mt-4">
                                     <Label>Teléfono</Label>
-                                    <Input />
+                                    <Input name="name" placeholder="Ingresa tu telefono" />
                                 </div>
                             </div>
-                            <div className="py-10 lg:py-0 lg:h-40 flex flex-col items-center justify-between">
+                            <div className="py-10 lg:py-0 lg:h-52 flex flex-col items-center justify-between">
                                 <Separator className="lg:block hidden" orientation="vertical" />
                                 <h1>o</h1>
                                 <Separator className="lg:block hidden" orientation="vertical" />
                             </div>
-                            <div>
+                            {login ? <div>
                                 <h2 className="font-bold text-lg mb-4">Ingresa a tu cuenta RIFAVO</h2>
                                 <div>
                                     <Label>Correo electrónico</Label>
@@ -246,9 +311,38 @@ const SorteoDetail = () => {
                                     <Label>Contraseña</Label>
                                     <Input onChange={handleForm} name="password" type="password" placeholder="Ingresa tu contraseña" />
                                 </div>
-                                <p className="text-sm mt-3">No tienes cuenta? <span className="cursor-pointer hover:underline">Registrate</span></p>
+                                <p className="text-sm mt-3">No tienes cuenta? <span className="cursor-pointer hover:underline" onClick={() => setLogin(false)}>Registrate</span></p>
                                 <Button onClick={authenticate} className="w-full mt-5">Ingresar</Button>
-                            </div>
+                            </div> :
+                                <div>
+                                    <h2 className="font-bold text-lg mb-4">Registrate en RIFAVO</h2>
+                                    <div className="mt-4">
+                                        <Label>Nombre</Label>
+                                        <Input onChange={handleForm} name="name" placeholder="Ingresa tu nombre" />
+                                    </div>
+                                    <div className="mt-4">
+                                        <Label>Apellido</Label>
+                                        <Input onChange={handleForm} name="lastname" placeholder="Ingresa tu apellido" />
+                                    </div>
+                                    <div className="mt-4">
+                                        <Label>Telefono</Label>
+                                        <Input onChange={handleForm} name="phone" placeholder="Ingresa tu telefono" />
+                                    </div>
+                                    <div className="mt-4">
+                                        <Label>Correo electrónico</Label>
+                                        <Input onChange={handleForm} name="email" placeholder="Ingresa tu correo" />
+                                    </div>
+                                    <div className="mt-4">
+                                        <Label>Contraseña</Label>
+                                        <Input onChange={handleForm} name="password" type="password" placeholder="Ingresa tu contraseña" />
+                                    </div>
+                                    <div className="mt-4">
+                                        <Label>Repetir contraseña</Label>
+                                        <Input onChange={handleForm} name="password2" type="password" placeholder="Repite tu contraseña" />
+                                    </div>
+                                    <p className="text-sm mt-3">Ya tienes cuenta? <span className="cursor-pointer hover:underline" onClick={() => setLogin(true)}>Ingresa</span></p>
+                                    <Button onClick={registroUsuario} className="w-full mt-5">Registrarme</Button>
+                                </div>}
                         </div></>}
                     <Separator className="mt-6" />
                     <div className="flex flex-col lg:flex-row lg:mt-6 mt-10 justify-between">
