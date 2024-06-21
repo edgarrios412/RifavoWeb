6b8ad2013e6ca663f226a21eb8a2506b1c863fc9
unknown <edgarrios412@gmail.com>
2024-06-20 19:47:55 -0500

    Se arreglaron errores en el responsive

--- src/pages/Apertura.jsx
+++ src/pages/Apertura.jsx
@@ -7,16 +7,16 @@ const Apertura = () => {
     const [s, m, h, d] = useCountdown(new Date(2024, 6, 1))
 
     return (
-        <div className="flex items-center justify-center h-[100vh] overflow-x-hidden relative">
+        <div className="flex items-center justify-center sm:h-[100vh] overflow-x-hidden relative">
             <div class="orange-blur"></div>
             <div class="orange-blur2 invisible lg:visible"></div>
             {/* <div class="orange-blur3"></div> */}
-            <div className="flex flex-col justify-center px-8 sm:px-0 py-20 w-full sm:w-[80%] md:w-[70%] lg:w-[60%] xl:w-[40%]">
+            <div className="flex flex-col justify-center px-8 sm:px-0 py-4 w-full sm:w-[80%] md:w-[70%] lg:w-[60%] xl:w-[40%]">
                 <div className="w-72 sm:w-96 my-10">
                     <Rifavo />
                 </div>
-                <h1 className="text-6xl font-bold">Llegaremos muy pronto</h1>
-                <p className="mt-4 text-slate-500 text-lg">Queremos que tengas una buena experiencia usando nuestra plataforma así que estamos haciendo lo mejor para tener una plataforma que puedas usar facilmente</p>
+                <h1 className="text-4xl sm:text-6xl font-bold">Llegaremos muy pronto</h1>
+                <p className="mt-4 text text-slate-500 text-normal sm:text-lg">Queremos que tengas una buena experiencia usando nuestra plataforma así que estamos haciendo lo mejor para tener una plataforma que puedas usar facilmente</p>
                 <div className="grid grid-cols-2 sm:grid-cols-4 gap-4 my-10 justify-items-center">
                     <div className="w-32 h-32 border border-slate-300 rounded-full flex flex-col items-center justify-center">
                         <p className="text-3xl">{d}</p>
@@ -36,7 +36,7 @@ const Apertura = () => {
                     </div>
                 </div>
                 <div className="flex justify-between gap-5 mt-3">
-                    <p className="text-slate-500">Mantente al tanto a través de nuestras redes sociales</p>
+                    <p className="text-slate-500 text-sm sm:text-base">Mantente al tanto a través de nuestras redes sociales</p>
                     <div className="flex gap-5">
                         <a href="https://web.facebook.com/profile.php?id=61560852461961" target="_blank">
                             <Facebook />
